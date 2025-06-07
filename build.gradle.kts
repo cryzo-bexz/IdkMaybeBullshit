7f8872e07681e63417a47da8725a5f261959da50
cryzo-bexz <testmebro4@gmail.com>
2025-06-08 01:10:28 +0530

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("com.aliucord.plugin") version "3.2.2"
+}
+
+plugin {
+    name = "HiCommand"
+    description = "Simple /hi command"
+    version = "1.0.0"
+    author = "YourName"
+    license = "MIT"
+}
